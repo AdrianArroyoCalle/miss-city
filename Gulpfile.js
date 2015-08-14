0d894af533d58d3210e0e2f9be00898147ecfb5c
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2015-08-14 17:37:29 +0200

    Basic skeleton

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+var gulp=require("gulp");
+var uglify=require("gulp-uglify");
+var rimraf=require("rimraf");
+var imagemin=require("gulp-imagemin");
+var minifyCSS=require("gulp-minify-css");
+var fs=require("fs");
+var EasyZip=require("easy-zip").EasyZip;
+var filesize=require("filesize");
+
+gulp.task("clean",function(cb){
+  rimraf("build",cb);
+});
+
+gulp.task("javascript",function(){
+  return gulp.src("src/index.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("build/"));
+});
+
+gulp.task("html",function(){
+  return gulp.src("src/index.html")
+    .pipe(gulp.dest("build/"));
+});
+
+gulp.task("css",function(){
+  return gulp.src("src/index.css")
+    .pipe(minifyCSS())
+    .pipe(gulp.dest("build/"));
+});
+
+gulp.task("zip",function(cb){
+  var zip=new EasyZip();
+  zip.zipFolder("build/",function(){
+    zip.writeToFile("miss-city.zip",cb);
+  },{rootFolder: "/"});
+});
+
+gulp.task("check-size",function(cb){
+  var stats=fs.statSync("miss-city.zip");
+  var size=stats["size"];
+  console.log("ZIP size is: "+filesize(size));
+  if(filesize(size,{output: "array"})[0] > 13){
+    console.log("Size is over 13kb. Please clean!!");
+  }
+  cb();
+});
+
+gulp.task("images",function(){
+  return gulp.src("res/*.gif")
+    .pipe(imagemin())
+    .pipe(gulp.dest("build/"));
+});
+
+gulp.task("default",["clean","javascript","html","css","images","zip","check-size"]);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+function main(){
+  console.log("MAIN");
+}
+
+window.addEventListener("load",main);
