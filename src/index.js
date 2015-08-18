7e3d986c5ee7bc7f24b2bd309510fd4ea7074ec4
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2015-08-18 18:01:21 +0200

    City, dron and GIFs

--- src/index.js
+++ src/index.js
@@ -8,31 +8,37 @@ function write(str){
 }
 
 var keys=[0,0,0,0];
-var box;
-var ctx;
+var box, ctx;
+var guy, guyY;
+var rotation=0;
+var dron=new Image;
+dron.src="dron.gif";
+var boy= new Image, girl=new Image;
+boy.src="boy.gif";
+girl.src="girl.gif";
 var city=[
-  0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, // 25
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, // 25
+  2,2,2,2,2,3,1,2,2,2,2,2,2,2,2,3,3,4,4,4,4,1,4,4,4,
+  2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,3,3,4,4,4,4,1,4,4,4,
+  2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,3,3,3,3,3,3,1,4,4,4,
+  1,1,1,1,1,1,1,2,2,2,3,3,3,3,1,3,3,3,3,3,3,1,4,4,4,
+  2,2,2,2,2,2,1,2,2,2,3,3,3,3,1,1,1,1,1,1,1,1,4,4,4,
+  2,2,3,3,2,2,1,1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,
+  3,3,3,3,3,3,1,0,1,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,
   1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, // 9 - the half
-  0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,
-  0,0,0,0,0,0,1,1,1,0,0,1,1,0,0,0,0,1,0,0,0,0,1,0,0,
-  0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,1,1,1,1,0,0,
-  0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,
-  0,0,1,1,1,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,
-  0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,
-  0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,
-  1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0
+  4,4,4,4,4,4,1,0,1,2,2,2,1,3,3,3,3,1,2,2,2,2,1,4,4,
+  4,4,4,4,4,4,1,1,1,2,2,1,1,3,3,3,3,1,2,2,2,2,1,4,4,
+  4,4,4,4,4,4,3,1,2,2,2,1,2,2,2,1,1,1,1,1,1,1,1,4,4,
+  4,4,4,4,4,4,3,1,2,2,2,1,2,2,2,1,2,2,2,2,2,2,2,4,4,
+  4,4,4,4,4,4,3,1,1,1,1,1,2,2,2,1,2,2,2,2,2,2,2,4,4,
+  3,3,1,1,1,1,1,1,2,2,2,1,1,1,1,1,2,2,4,4,4,4,4,4,4,
+  4,4,1,2,2,2,2,3,2,2,2,4,4,4,4,1,2,2,4,4,4,4,4,4,4,
+  3,3,1,2,2,2,2,3,0,0,0,4,4,4,4,1,1,1,1,1,1,1,1,1,1,
+  1,1,1,2,2,2,2,3,0,0,0,0,0,0,0,1,4,4,4,4,4,4,4,4,4
 ];
 
 function key(evt,state){
-  switch(evt.keyCode){
+  switch(evt.key){
     case "w" : keys[0]=state;break;
     case "s" : keys[1]=state;break;
     case "a" : keys[2]=state;break;
@@ -57,7 +63,7 @@ function loop(){
   // Render
   var draw=city.map(function(val){
     switch(val){
-      case 0: return "rgba(91,196,124,0)";
+      case 0: return "rgba(91,196,124,1)";
       case 1: return "rgb(76, 77, 76)";
       case 2: return "rgb(84, 230, 54)";
       case 3: return "rgb(37, 88, 219)";
@@ -74,6 +80,35 @@ function loop(){
       y+=box;
     }
   });
+  if(keys[0]){
+    if(guyY-1 > 0){
+      guyY--;
+    }
+  }
+  if(keys[1]){
+    if(guyY+1 < id("a").height){
+      guyY++;
+    }
+  }
+  if(keys[2]){
+    if(guyX-1 > 0){
+      guyX--;
+    }
+  }
+  if(keys[3]){
+    if(guyX+1 < id("a").width){
+      guyX++;
+    }
+  }
+
+  /*ctx.fillStyle="black";
+  ctx.beginPath();
+  ctx.arc(guyX,guyY,box*1/3,0,Math.PI*2,true);
+  ctx.closePath();
+  ctx.fill();*/
+  ctx.drawImage(dron,guyX,guyY,box*2/3,box*2/3);
+
+  requestAnimationFrame(loop);
 }
 
 function main(){
@@ -82,9 +117,11 @@ function main(){
   write("-----------------------------------------------");
   write("Look! It's Euralia. It's like the perfect city. Darío Paz is the perfect mayor... but not for us");
   write("Our company, Encisa, is trying to build a super mall in an old house. The mayor however has promised that if he wins the elections, he will do a library");
-  write("We must stop it! You have a week (15 minutes) to win the elections. Do whatever you want!");
+  write("We must stop it! You have a week (15 minutes) to win the elections. Use our dron!!");
   id("a").height=id("a").width*3/4;
   box=id("a").width/25;
+  guyX=box;
+  guyY=box;
   ctx=id("a").getContext("2d");
   ctx.fillStyle="rgb(91, 196, 124)";
   ctx.fillRect(0,0,id("a").width,id("a").width*3/4);
@@ -101,6 +138,7 @@ function main(){
     if(id("t").value==15){
       // END GAME
       write("Elections day! - Darío Paz continues as mayor. You have lost!! Maybe trying again...");
+      clearInterval(time);
     }
   },1000*60);
 
