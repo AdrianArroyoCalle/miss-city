b791c5cf45c40d99a75cdf9d0b620b0535b891a7
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2015-09-04 18:17:47 +0200

    Better layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 function id(i){
   return document.getElementById(i);
 }
-function write(str){
-  var scroll=id("m");
-  scroll.value+="\n"+str;
-  scroll.scrollTop=scroll.scrollHeight;
-}
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -13,6 +9,8 @@ function getRandomInt(min, max) {
 var keys=[0,0,0,0];
 var money=200000;
 var gameOver=false;
+var time, t=0, v=0;
+var lock=true;
 var box, ctx;
 var guyX, guyY;
 var lastCalledTime;
@@ -54,6 +52,28 @@ var city=[
   1,1,1,2,2,2,2,3,0,0,0,0,0,0,0,1,4,4,4,4,4,4,4,4,4
 ];
 
+function write(str,cb){
+  /*var scroll=id("m");
+  scroll.value+="\n"+str;
+  scroll.scrollTop=scroll.scrollHeight;*/
+  /* use custom message panel */
+  
+  ctx.fillStyle="rgb(0,0,255)";
+  ctx.fillRect(box*3,box*3,box*18,box*4);
+  ctx.fillStyle="black";
+  // ctx.font
+  ctx.fillText(str,box*3,box*4);
+  
+  // BLOCK GAME
+  lock=true;
+  var cl=setInterval(function(){
+	if(!lock){
+		clearInterval(cl);
+		cb();
+	}  
+  },100);
+}
+
 function calculateRandomRoad(){
   var roads=city.filter(function(tile){
 	  return (tile === 1) ? true : false;
@@ -80,12 +100,7 @@ function key(evt,state){
     case "d" : keys[3]=state;break;
   }
 }
-
-function loop(){
-  var speed=box*2; // 2 box per second
-  var delta=(new Date().getTime() - lastCalledTime)/1000;
-  lastCalledTime=Date.now();
-  id("z1").onclick=function(){
+function z1(){
 	// SPREAD ADVERTISING
 	actionRadio={
 		prop: 30,
@@ -98,8 +113,9 @@ function loop(){
 		y: guyY,
 		price: 2000
 	};
-  }
-  id("z2").onclick=function(){
+}
+
+function z2(){
 	// FREE STICKERS
 	actionRadio={
 		prop: 20,
@@ -113,7 +129,8 @@ function loop(){
 		price: 1500
 	};
   }
-  id("z3").onclick=function(){
+  
+function z3(){
 	actionRadio={
 		prop: 60,
 		show: true,
@@ -126,7 +143,8 @@ function loop(){
 		price: 4000
 	};
   }
-  id("z4").onclick=function(){
+  
+function z4(){
 	actionRadio={
 		prop: 5,
 		show: true,
@@ -138,7 +156,15 @@ function loop(){
 		y: guyY,
 		price: 2500
 	};
+}
+function loop(){
+  if(lock){
+	requestAnimationFrame(loop);
+	return;
   }
+  var speed=box*2; // 2 box per second
+  var delta=(new Date().getTime() - lastCalledTime)/1000;
+  lastCalledTime=Date.now();
 
   // Render
   ctx.fillStyle="rgb(91, 196, 124)";
@@ -195,7 +221,6 @@ function loop(){
   ctx.arc(guyX,guyY,box*1/3,0,Math.PI*2,true);
   ctx.closePath();
   ctx.fill();*/
-  ctx.drawImage(dron,guyX,guyY,box*2/3,box*2/3);
 
   // Simulate people
   if(getRandomInt(0,100+1)<3){
@@ -239,6 +264,8 @@ function loop(){
     }
   });
   
+  ctx.drawImage(dron,guyX,guyY,box*2/3,box*2/3);
+  
   // Draw Action Radio
   if(actionRadio.show){
 	ctx.strokeStyle=actionRadio.color;
@@ -255,6 +282,7 @@ function loop(){
 		if(money < 0){
 			write("You have wasted all the money!");
 			gameOver=true;
+			clearInterval(time);
 		}
 		var filtered=people.map(function(pp){
 			if(pp.show && (actionRadio.x-actionRadio.radius) < pp.x*box && pp.x*box < (actionRadio.x+actionRadio.radius)){
@@ -290,31 +318,72 @@ function loop(){
   if(votes > 50){
 	// WIN
 	gameOver=true;
-	write("Yeah!! You have more than a half votes. You can be the next mayor of the city. You have scored: "+money+" points");  
+	write("Yeah!! You have more than a half votes. You can be the next mayor of the city. You have scored: "+money+" points");
+	clearInterval(time);
   }
   
-  id("v").value=votes;
-  id("c").value=money;
+  v=votes;
+  
+  // DRAW HUD
+  
+  ctx.lineWidth=box*1/10;
+  
+  ctx.fillStyle="green";
+  ctx.strokeStyle="black";
+  ctx.strokeRect(0,box*18,box*5,box*3/4);
+  ctx.fillRect(0,box*18,(box*5*t)/120,box*3/4);
+  ctx.fillStyle="black";
+  ctx.fillText("Time",0,box*18);
+  
+  ctx.fillStyle="red";
+  ctx.strokeStyle="black";
+  ctx.strokeRect(box*5,box*18,box*5,box*3/4);
+  ctx.fillRect(box*5,box*18,(box*5*v)/51,box*3/4);
+  ctx.fillStyle="black";
+  ctx.fillText("Votes",box*5,box*18);
+  
+  ctx.fillStyle="pink";
+  ctx.strokeStyle="black";
+  ctx.strokeRect(box*10,box*18,box*5,box*3/4);
+  ctx.fillRect(box*10,box*18,(box*5*money)/200000,box*3/4);
+  ctx.fillStyle="black";
+  ctx.fillText("Money",box*10,box*18);
+  
+  ctx.fillText("WASD - Move, VBNM - Different attacks",box*15,(box*18)+10);
 
   if(!gameOver)
 	requestAnimationFrame(loop);
 }
 
 function main(){
+	// SCREEN 360x640 is MINIMUM
   // Sound?
-  id("m").value="";
-  write("Welcome to MissCity - The reversed SsimCity - js13k");
-  write("-----------------------------------------------");
-  write("Look! It's Euralia. It's like the perfect city. Darío Paz is the perfect mayor... but not for us");
-  write("Our company, Encisa, is trying to build a super mall in an old house. The mayor however has promised that if he wins the elections, he will do a library");
-  write("We must stop it! You have a week (2 minutes) to win the elections. Use our dron!!");
-  id("a").height=id("a").width*3/4;
+  ctx=id("a").getContext("2d");
+  //id("a").height=id("a").width*3/4;
+  id("a").height=window.innerHeight;
+  id("a").width=window.innerHeight/(3/4);
   box=id("a").width/25;
   guyX=box;
   guyY=box;
-  ctx=id("a").getContext("2d");
+  
   ctx.fillStyle="rgb(91, 196, 124)";
   ctx.fillRect(0,0,id("a").width,id("a").width*3/4);
+  //id("a").height+=75;
+  
+  //ctx.fillStyle="rgba(47,87,220,0.8)";
+  //ctx.fillRect(0,id("a").height*3/4,id("a"),75);
+  
+  write("Welcome to MissCity - The reversed SsimCity - js13k",function(){
+	  write("Look! It's Euralia. It's like the perfect city. Darío Paz is the perfect mayor... but not for us",function(){
+		  write("Our company, Encisa, is trying to build a super mall in an old house. ",function(){
+			  write("The mayor however has promised that if he wins the elections, he will do a library",function(){
+				  write("We must stop it! You have a week (2 minutes) to win the elections. Use our dron!!",function(){
+					  
+				});
+			  });
+		  });
+	  });
+  });
 
   // Draw city
   // 0 is nothing
@@ -323,27 +392,29 @@ function main(){
   // 3 - Blue are stores
   // 4 - Orange is industry
 
-  var time=setInterval(function(){
-    id("t").value+=1;
-    write("Day completed. Money - "+money+ " | Votes - "+id("v").value+ "%");
-    if(id("t").value==2){
+  time=setInterval(function(){
+    t+=1;
+    console.log("Money - "+money+ " | Votes - "+v+ "%");
+    if(t==120){
       // END GAME
       write("Elections day! - Darío Paz continues as mayor. You have lost!! Maybe trying again...");
       gameOver=true;
       clearInterval(time);
     }
-  },1000*60);
+  },1000);
 
   window.onkeyup=function(evt){
+	if(lock)
+		lock=false;
     key(evt,0);
   }
   window.onkeydown=function(evt){
     key(evt,1);
     switch(evt.key){
-		case "v": id("z1").click();break;
-		case "b": id("z2").click();break;
-		case "n": id("z3").click();break;
-		case "m": id("z4").click();break;
+		case "v": z1();break;
+		case "b": z2();break;
+		case "n": z3();break;
+		case "m": z4();break;
 	}
   }
 
